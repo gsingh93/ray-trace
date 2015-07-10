f7273f3fe829a7faaad4a5dc70ab1d41d3e98c45
Gulshan Singh <gsingh2011@gmail.com>
2015-07-09 21:13:11 -0400

    Fixed intersection bug

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 extern crate image;
 extern crate nalgebra;
 
-use std::fs::File;
+use image::{ImageBuffer, Rgb, Pixel};
 
-use image::ColorType;
-use image::png::PNGEncoder;
-
-use nalgebra::{cross, dot, normalize};
+use nalgebra::{cross, dot, normalize, Norm};
 
 type Vec3 = nalgebra::Vec3<f32>;
 
@@ -21,11 +18,12 @@ struct Ray {
 
 impl Ray {
     fn new(origin: Vec3, dir: Vec3) -> Self {
-        Ray { origin: origin, dir: dir }
+        Ray { origin: origin, dir: normalize(&dir) }
     }
 }
 
 #[allow(dead_code)]
+#[derive(Debug)]
 struct Intersection {
     pos: Vec3,
 }
@@ -47,11 +45,15 @@ struct Sphere {
 
 impl Surface for Sphere {
     fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        let dist = ray.origin - self.pos;
-        let determinant = dot(&ray.dir, &dist).powi(2) - dot(&dist, &dist) + self.radius.powi(2);
-        if determinant > 0. {
-            // FIXME: Is this always the right solution to take?
-            let d = -dot(&ray.dir, &dist) - determinant;
+        let center_offset = self.pos - ray.origin;
+        let b = 2. * dot(&ray.dir, &center_offset);
+        let c = center_offset.sqnorm() - self.radius * self.radius;
+
+        let discriminant = b * b - 4. * c;
+
+        if discriminant > 0. {
+            // TODO: What about the other solution?
+            let d = -b - discriminant.sqrt();
             let pos = ray.origin + ray.dir * d;
             Some(Intersection::new(pos))
         } else {
@@ -86,8 +88,8 @@ impl Camera {
     }
 
     fn get_ray(&self, x: u32, y: u32) -> Ray {
-        let norm_x = (x / WIDTH) as f32 - 0.5;
-        let norm_y = (y / HEIGHT) as f32 - 0.5;
+        let norm_x = (x as f32 / WIDTH as f32) - 0.5;
+        let norm_y = (y as f32 / HEIGHT as f32) - 0.5;
 
         let dir = self.right * norm_x + self.up * norm_y + self.dir;
         Ray::new(self.pos, dir)
@@ -95,17 +97,14 @@ impl Camera {
 }
 
 fn main() {
-    const BUF_SIZE: usize = (WIDTH * HEIGHT) as usize;
-
-    let mut f = File::create(OUT_FILE).unwrap();
-    let mut encoder = PNGEncoder::new(&mut f);
-    let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
+    let sphere_color: Rgb<u8> = Rgb::from_channels(255, 0, 0, 1);
+    let mut im: ImageBuffer<Rgb<u8>, _> = ImageBuffer::new(WIDTH, HEIGHT);
 
     let camera = {
-        let pos = Vec3::new(0., 0., -2.);
-        let dir = Vec3::new(0., 0., 0.);
+        let pos = Vec3::new(0., 0., -4.);
+        let lookat = Vec3::new(0., 0., 0.);
         let up = Vec3::new(0., 1., 0.);
-        Camera::from_lookat(pos, dir, up)
+        Camera::from_lookat(pos, lookat, up)
     };
     let sphere = Sphere::new(Vec3::new(0., 0., 0.), 1.);
 
@@ -113,10 +112,10 @@ fn main() {
         for y in 0..HEIGHT {
             let ray = camera.get_ray(x, y);
             if let Some(_) = sphere.intersect(&ray) {
-                let idx = (y * WIDTH + x) as usize;
-                buf[idx] = 100;
+                im.put_pixel(x, y, sphere_color);
             }
         }
     }
-    encoder.encode(&buf, WIDTH, HEIGHT, ColorType::RGB(8)).unwrap();
+
+    im.save(OUT_FILE).unwrap();
 }
